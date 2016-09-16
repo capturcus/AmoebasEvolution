14e405331853a47dff27434151f6494432e84c31
marcin@flondra <consommepanchi@gmail.com>
2016-09-16 21:30:56 +0200

    progress

--- main.cpp
+++ main.cpp
@@ -4,23 +4,28 @@
 const float32 TIME_STEP = 1.f / 60.f;
 const int POSITION_ITERATIONS = 3;
 const int VELOCITY_ITERATIONS = 8;
+const float32 LINEAR_DAMPING = 6;
+const float32 ANGULAR_DAMPING = 6;
 
 class Amoeba {
 public:
-    static Amoeba *createAmoeba(b2World *boxWorld) {
+    static Amoeba *createAmoeba(b2World *boxWorld, b2Vec2 &position) {
         auto bodyDef = new b2BodyDef;
         bodyDef->type = b2BodyType::b2_dynamicBody;
         bodyDef->active = true;
+        bodyDef->position = position;
         b2Body *body = boxWorld->CreateBody(bodyDef);
         b2Shape *shape;
         shape = new b2CircleShape;
-        shape->m_radius = .5f;
+        shape->m_radius = 1.f;
         b2FixtureDef *fixtureDef = new b2FixtureDef;
-        fixtureDef->density = 1;
+        fixtureDef->density = .1;
         fixtureDef->friction = 1;
         fixtureDef->restitution = 1;
         fixtureDef->shape = shape;
         body->CreateFixture(fixtureDef);
+        body->SetLinearDamping(LINEAR_DAMPING);
+        body->SetAngularDamping(ANGULAR_DAMPING);
         delete fixtureDef;
         Amoeba *ret = new Amoeba();
         ret->boxBodyDef = bodyDef;
@@ -46,8 +51,8 @@ struct AmoebasWorld {
         delete boxWorld;
     }
 
-    Amoeba *createAmoeba() {
-        return Amoeba::createAmoeba(boxWorld);
+    Amoeba *createAmoeba(b2Vec2 &position) {
+        return Amoeba::createAmoeba(boxWorld, position);
     }
 
     void step() {
@@ -60,52 +65,71 @@ sf::CircleShape amoebaEye(0.2f);
 
 void drawAmoebaAtPos(sf::RenderWindow *window, b2Vec2 position, float rotation) {
     amoebaBody.setPosition(position.x, position.y);
-    amoebaEye.setPosition((float) (position.x + 0.8f + 0.8*cos(rotation)), (float) (position.y + 0.8f + 0.8*sin(rotation)));
+    amoebaEye.setPosition((float) (position.x + 0.8f + 0.8 * cos(rotation)),
+                          (float) (position.y + 0.8f + 0.8 * sin(rotation)));
     window->draw(amoebaBody);
     window->draw(amoebaEye);
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::View view(sf::Vector2f(100, 100), sf::Vector2f(10, 10));
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    window.setFramerateLimit(60);
+    sf::View view(sf::Vector2f(0, 0), sf::Vector2f(20, 16));
     amoebaEye.setFillColor(sf::Color::Black);
     amoebaBody.setFillColor(sf::Color::Green);
 
     AmoebasWorld world;
 
-    float testRot = 0;
+    b2Vec2 vec;
+    vec.Set(0, 1.2f);
+    Amoeba *a = world.createAmoeba(vec);
+    vec.Set(5, 0);
+    Amoeba *a2 = world.createAmoeba(vec);
 
     while (window.isOpen()) {
         sf::Event event;
+        world.step();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            view.move(-.1f, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            view.move(.1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            view.move(0, -.1f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            view.move(0, .1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add)) {
+            view.zoom(0.99);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract)) {
+            view.zoom(1.01);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            a->boxBody->ApplyForceToCenter(b2Vec2(
+                    (float32) (cos(a->boxBody->GetAngle()) * 10),
+                    (float32) (sin(a->boxBody->GetAngle()) * 10)
+            ), true);
+            a->boxBody->ApplyTorque(2, true);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            a->boxBody->ApplyForceToCenter(b2Vec2(
+                    (float32) (cos(a->boxBody->GetAngle()) * 10),
+                    (float32) (sin(a->boxBody->GetAngle()) * 10)
+            ), true);
+            a->boxBody->ApplyTorque(-2, true);
+        }
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Left) {
-                    view.move(-1, 0);
-                }
-                if (event.key.code == sf::Keyboard::Right) {
-                    view.move(1, 0);
-                }
-                if (event.key.code == sf::Keyboard::Up) {
-                    view.move(0, -1);
-                }
-                if (event.key.code == sf::Keyboard::Down) {
-                    view.move(0, 1);
-                }
-                if(event.key.code == sf::Keyboard::H){
-                    testRot -= 0.1;
-                }
-                if (event.key.code == sf::Keyboard::L){
-                    testRot += 0.1;
-                }
-            }
         }
 
         window.setView(view);
         window.clear();
-        drawAmoebaAtPos(&window, b2Vec2(101, 101), testRot);
-        drawAmoebaAtPos(&window, b2Vec2(99,99), testRot+1);
+        drawAmoebaAtPos(&window, a->boxBody->GetPosition(), a->boxBody->GetAngle());
+        drawAmoebaAtPos(&window, a2->boxBody->GetPosition(), a2->boxBody->GetAngle());
         window.display();
     }
 
